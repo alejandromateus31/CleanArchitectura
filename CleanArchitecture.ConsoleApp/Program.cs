ef4b8744d5c0c7575c40e6eb78e0bc99c8d19fd3
Diego Mateus <alejandromateus31@outlook.com>
2023-07-04 11:56:45 -0500

    Primer commit

---
+++ CleanArchitecture.ConsoleApp/Program.cs
@@ -0,0 +1,62 @@
+﻿using CleanArchitecture.Data;
+using CleanArchitecture.Domian;
+
+StreamerDbContext dbContext = new();
+
+void QueryStreaming()
+{
+    var streamers = dbContext!.Streamers!.ToList();
+    foreach (var streame in streamers)
+    {
+        Console.WriteLine($"{streame.Id} - {streame.Nombre}");
+    }
+}
+
+
+async Task AddNewRecords()
+{
+
+
+    Streamer streamer = new()
+    {
+        Nombre = "Amazon Prime",
+        Url = "https://www.AmazonPrime.com"
+    };
+
+    dbContext!.Streamers!.Add(streamer);
+    await dbContext.SaveChangesAsync();
+
+    var movies = new List<Video>
+{
+    new Video
+    {
+        Nombre = "Mad Max",
+        StreamerId = streamer.Id
+    },
+    new Video
+    {
+        Nombre = "Batman ",
+        StreamerId = streamer.Id
+    },
+    new Video
+    {
+        Nombre = "Crepusculo",
+        StreamerId = streamer.Id
+    },
+    new Video
+    {
+        Nombre = "Citizen Kane",
+        StreamerId = streamer.Id
+    }
+
+};
+
+    await dbContext.AddRangeAsync(movies);
+    await dbContext.SaveChangesAsync();
+
+}
+
+
+
+
+
