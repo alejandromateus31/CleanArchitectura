8c587c37ec5fbdbd02f8361f1e77fe6eacc0bb4b
Diego Mateus <alejandromateus31@outlook.com>
2023-07-04 16:35:28 -0500

    Link y Ef - Tracking - Not Tracking

--- CleanArchitecture.ConsoleApp/Program.cs
+++ CleanArchitecture.ConsoleApp/Program.cs
@@ -7,11 +7,47 @@ StreamerDbContext dbContext = new();
 
 //await AddNewRecords();
 //QueryStreaming();
-
-await QueryFilter();
-
+//await QueryFilter();
+//await QueryMethods();
+await QueryLinq();
 Console.WriteLine("Presione cualquier tecla para terminar el programa");
 Console.ReadKey();
+
+
+async Task QueryMethods() 
+{
+
+    var streamer = dbContext!.Streamers!;
+
+
+    var firstAsync = await streamer.Where( x => x.Nombre.Contains("a")).FirstAsync();
+    Console.WriteLine(firstAsync.Id);
+    var firstOrdefaultAsync = await streamer.Where(x => x.Nombre.Contains("a")).FirstOrDefaultAsync();
+    Console.WriteLine(firstOrdefaultAsync.Id);
+    var firstAsync_v2 = await streamer.FirstOrDefaultAsync(x => x.Nombre.Contains("a"));
+    Console.WriteLine(firstAsync_v2.Id);
+    var singleAsync = await streamer.Where(x => x.Id == 1).SingleAsync();
+    Console.WriteLine(singleAsync.Id);
+    var singleOrDefaultAsync = await streamer.Where(x => x.Id == 1).SingleAsync();
+    Console.WriteLine(singleOrDefaultAsync.Id);
+    var resultado = await streamer.FindAsync(3);
+    Console.WriteLine(resultado.Id);
+}
+
+async Task QueryLinq() 
+{
+    Console.WriteLine($"Ingrese el servicio de streaming");
+    var stringName = Console.ReadLine();
+    var streamers = await (from i in dbContext.Streamers 
+                           where EF.Functions.Like(i.Nombre , $"%{stringName}%") select i).ToListAsync();
+
+    foreach (var streamer in streamers)
+    {
+        Console.WriteLine($"{streamer.Id} - {streamer.Nombre}");
+    
+    }
+
+}
 
 
 async Task QueryFilter()
